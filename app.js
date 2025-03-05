ff8e51a7a110e246e365d1e89e8b98651ba1266b
nathan-nigussie <nathantolosa11@gmail.com>
2025-03-05 19:07:36 +0200

    added

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port=3000;
 
 // Root route
 app.get('/',(req,res) => {
-    res.send('<h1>Express Demo App</h1><h4>Message: Success</h4><p>Version 1.0</p>');
+    res.send('<h1>Wellcome to Nathan Express Demo App</h1><h4>Message: Success</h4><p>Version 1.0</p>');
 });
 
 // Products route
